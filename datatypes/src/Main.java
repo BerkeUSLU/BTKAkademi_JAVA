724c59310df0cbe578340b3d39798e20edfe6e88
Berke <berkeuslu0@gmail.com>
2023-02-25 15:22:27 +0300

    -modules are updated!

---
+++ datatypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        double sayi = 12.5;
+
+        sayi = -129;
+
+        char karakter = 'A';
+
+        boolean dogruMu = false;
+
+        System.out.println(sayi);
+        System.out.println(karakter);
+        System.out.println(dogruMu);
+    }
+}
