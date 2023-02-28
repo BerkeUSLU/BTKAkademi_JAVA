ca1833f249f385e99de3f145a946b43cb0a5a83d
Berke <berkeuslu0@gmail.com>
2023-02-28 13:47:09 +0300

    -multiDimensionalArray is created & updated.

---
+++ multiDimensionalArrayDemo/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    // A program that uses multiDimensionalArray and a loop created to search them in order.
+    public static void main(String[] args) {
+        String[][] sehirler = new String[3][3];
+
+        sehirler[0][0] = "İstanbul";
+        sehirler[0][1] = "Bursa";
+        sehirler[0][2] = "Antalya";
+        sehirler[1][0] = "Los Angeles";
+        sehirler[1][1] = "New York";
+        sehirler[1][2] = "Seattle";
+        sehirler[2][0] = "Vancouver";
+        sehirler[2][1] = "Virginia Beach";
+        sehirler[2][2] = "Ankara";
+
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                System.out.println(sehirler[i][j]);
+            }
+        }
+    }
+}
