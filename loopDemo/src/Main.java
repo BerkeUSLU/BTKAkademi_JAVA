9bbbfcef6e648a8d7b0c36286076de89d2e7be71
Berke <berkeuslu0@gmail.com>
2023-02-28 12:54:09 +0300

    -loopDemo is updated.

--- loopDemo/src/Main.java
+++ loopDemo/src/Main.java
@@ -4,5 +4,13 @@ public class Main {
         for (int i = 2; i < 10; i += 2 ) {
             System.out.println(i);
         }
+        System.out.println("******************");
+        int i = 2;
+        //While
+        while (i < 10) {
+            System.out.println(i);
+            i+= 2;
+        }
+        System.out.println("While döngüsü bitti!");
     }
 }
