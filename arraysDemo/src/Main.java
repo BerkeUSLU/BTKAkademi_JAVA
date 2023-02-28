46635f0e69d9f4fb62c8e5ed57a1de6e4206d759
Berke <berkeuslu0@gmail.com>
2023-02-28 13:09:35 +0300

    -arraysDemo created & updated.

---
+++ arraysDemo/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        String ogrenci1 = "Engin";
+        String ogrenci2 = "Berke";
+        String ogrenci3 = "Selin";
+        String ogrenci4 = "Zeynep";
+
+        System.out.println(ogrenci1);
+        System.out.println(ogrenci2);
+        System.out.println(ogrenci3);
+        System.out.println(ogrenci4);
+
+        System.out.println("-------------------------");
+
+        String[] ogrenciler = new String[4];
+
+        ogrenciler[0] = "Engin";
+        ogrenciler[1] = "Berke";
+        ogrenciler[2] = "Selin";
+        ogrenciler[3] = "Zeynep";
+
+        for (int i = 0; i < ogrenciler.length; i++) {
+            System.out.println(ogrenciler[i]);
+        }
+
+        System.out.println("-------------------------");
+
+        for (String ogrenci : ogrenciler) {
+            System.out.println(ogrenci);
+        }
+
+    }
+}
