5ee552b8978ed3b0f4cd6cfa84f859544f835734
Berke USLU <berkeuslu0@gmail.com>
2023-03-03 13:54:32 +0300

    -stringsDemo is updated.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("BTKAkademi - JAVA İle Programlamaya Giriş");
+        System.out.println("***********************************************");
+        System.out.print("**");
+        System.out.print(" BTKAkademi - JAVA İle Programlamaya Giriş ");
+        System.out.print("**");
+        System.out.println("");
+        System.out.println("***********************************************");
     }
 }
