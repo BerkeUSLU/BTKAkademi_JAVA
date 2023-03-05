76c2e63cca5d7922615f38d6be987f28dd7bb3ea
Berke USLU <berkeuslu0@gmail.com>
2023-03-05 15:22:30 +0300

    -miniProjeAsalSayi is created & updated.

---
+++ miniProjeAsalSayi/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Lütfen bir sayı giriniz : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("");
+
+        int remainder = number % 2;
+
+        //System.out.println(remainder);
+
+        if (number <= 2) {
+            System.out.println("Geçersiz bir sayı giriş yaptının!");
+            System.out.print("Program sonlandırıldı!");
+            System.exit(1);
+        }
+
+        boolean isPrime = true;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " sayısı asaldır! ");
+        }
+        else {
+            System.out.println(number + " sayısı asal değildir! ");
+        }
+
+    }
+}
