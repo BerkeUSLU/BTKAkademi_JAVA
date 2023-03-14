eedad89e1850990d1c933bb41a0cf6abe3e89f6b
Berke USLU <berkeuslu0@gmail.com>
2023-03-14 17:45:10 +0300

    -mukemmelSayi is created

---
+++ mukemmelSayi/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // 6 --> 1,2,3
+        // A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
+
+    }
+}
